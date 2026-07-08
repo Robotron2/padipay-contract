96470a8cbd1c6d6f6fc7258aff39605c2691ea4e
Theophilus Adesola <theophilusadesola002@gmail.com>
2026-07-08 12:49:45 +0100

    feat: integrate Soroban Token Client

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,11 @@
 mod contract;
 pub mod error;
 pub mod storage;
+pub mod token;
 pub mod types;
 
 pub use contract::*;
 pub use error::*;
 pub use storage::*;
+pub use token::*;
 pub use types::*;
